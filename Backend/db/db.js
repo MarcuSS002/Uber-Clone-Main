c9c76af37efab08b2930f5b30a067f475f566e9a
MarcuSS002 <hyperxaxis@gmail.com>
2025-12-05 11:20:25 +0530

    Fix MongoDB connection URI and remove deprecated mongoose options

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 function connectToDb() {
     const mongoURI = process.env.MONGODB_URI;
-    mongoose.connect(mongoURI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
+    mongoose.connect(mongoURI)
     .then(() => console.log('MongoDB connected'))
     .catch(err => {
         console.error('MongoDB connect error:', err.message);
